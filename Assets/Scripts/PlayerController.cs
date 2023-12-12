3d0857b967ab9437e88f1d1eaf595d713e7ec4b4
celerywastaken <152607041+celerywastaken@users.noreply.github.com>
2023-12-12 15:54:08 +0100

    Lab 3.2
    Added Objects Movement
    Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
     public float zRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +43,11 @@ public class PlayerController : MonoBehaviour
         if (transform.position.z > zRange)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            //Launch a projectile from the Player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
